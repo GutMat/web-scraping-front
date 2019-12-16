188fc0f58147a5af2dae0a7f4436a4b77e1a3004
Gut Mateusz <gutmateusz96@interia.pl>
2019-12-16 18:26:21 +0100

    added form to enter the number  items to transform

--- src/containers/Transformator/Transformator.js
+++ src/containers/Transformator/Transformator.js
@@ -2,19 +2,32 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import ReactJson from 'react-json-view'
 import './Transformator.css'
-import { Spinner } from 'react-bootstrap';
+import { Form, Button, Spinner } from 'react-bootstrap';
 
 import Book from '../../components/Book/Book'
 
 class Transformator extends Component {
 
-    state = {
-        books: [],
-        isTransformated: false,
-        numberToTransform : 3
-    }
+    constructor(props) {
+        super(props);
+        this.state = {
+            books: [],
+            isEnteredValue: false,
+            isTransformated: false,
+            numberToTransform : 1
+        }
 
-    componentDidMount() {
+        this.handleValueChange = this.handleValueChange.bind(this);
+        this.toggleTransformatedItems = this.toggleTransformatedItems.bind(this);
+    }
+    
+    handleValueChange = (event) => {
+        this.setState({numberToTransform: event.target.value});
+     }
+
+    toggleTransformatedItems = () => {  
+        const doesEntered = this.state.isEnteredValue;
+        this.setState( { isEnteredValue: !doesEntered} );
         const doesTransformated = this.state.isTransformated;
         let link = "http://localhost:8081/getTransformedItems/" + this.state.numberToTransform;
         axios.get( link )
@@ -41,14 +54,29 @@ class Transformator extends Component {
 
     return (
         <div className="container View">
+
+            {this.state.isEnteredValue ? 
                 <div className="Books">
-                        {this.state.isTransformated ? books :
+                    {this.state.isTransformated ? books :
                         <div style={{textAlign: "center"}}>
                             <Spinner animation="border" role="status" />
                             <h4 style={{marginTop: '15px'}}>Transformating data...</h4>
                         </div>
-                        }
-                </div>     
+                    }
+            </div>: 
+            <Form onSubmit={this.toggleTransformatedItems}>
+                <Form.Group>
+                    <Form.Label><h3>Items number</h3></Form.Label>
+                    <Form.Control size="lg" type="number" min="1" max="120" placeholder="Enter the number of items" value={this.state.numberToTransform} onChange={this.handleValueChange}/>
+                    <Form.Text className="text-muted">
+                        Enter the number of items you wanna transform (1-120)
+                    </Form.Text>
+                </Form.Group>
+                <Button variant="dark" type="submit">
+                    Transform items
+                </Button>
+            </Form>}
+                 
             {/*<ReactJson src={this.state.books} theme="summerfruit" />*/}
         </div>
     );
